917d76d545fd75ca2004fe76aca651fdf7b33db8
Littlelinkrocks <77841378+Littlelinkrocks@users.noreply.github.com>
2021-05-02 21:10:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,94 @@
-var userScore = 0;
+// List of icons that can be used in the game
+var icons = ["icon://fa-hand-rock-o", "icon://fa-hand-paper-o", "icon://fa-hand-scissors-o"];
+
+// List of possible choices to play
+var choices = ["Rock", "Paper", "Scissors"];
+
+// User and computer scores, started at 0
+var playerScore = 0;
 var computerScore = 0;
 
-var computerChoice = Math.random();
+// Button for player to play rock
+onEvent("rockButton","click",function(){
+  updateScreen("Rock");
+});
 
-var computerChoice = computerChoice < 0.34 ? "rock" : ( computerChoice > 0.67 ? "scissors" : "paper" ) ;
+// Button for player to play paper
+onEvent("paperButton","click",function(){
+  updateScreen("Paper");
+});
 
-var mess = { 
-  rock : { scissors : 'You Win', paper : 'You lose'} ,
-  paper : { rock : 'You Win', scissors : 'You lose' },
-  scissors : { paper : 'You Win', rock : 'You lose' }
-}
-for (let step = 0; step < x; step++) {
-  var playerChoice = window.prompt("Do you choose rock, paper or scissors ?");
+// Button for player to play scissors
+onEvent("scissorsButton","click",function(){
+  updateScreen("Scissors");
+});
 
-var computerChoice = Math.random();
-
-var computerChoice = computerChoice < 0.34 ? "rock" : ( computerChoice > 0.67 ? "scissors" : "paper" ) ;
-
-var mess = { 
-  rock : { scissors : 'You Win', paper : 'You lose'} ,
-  paper : { rock : 'You Win', scissors : 'You lose' },
-  scissors : { paper : 'You Win', rock : 'You lose' }
-}
-
-if ( computerChoice === playerChoice)
-  result = "It's a tie!" ; 
-
-
-else
-  result = mess[playerChoice][computerChoice] ;
-
-console.log(  ' ( ' + result + ' ) ') ;
-if ( userScore > computerScore) {
+// When the user clicks any of the three buttons it runs a round
+// of rock paper scissors. This function determines who won the
+// game and updates the screen and scores for each player.
+function updateScreen(playerChoice){
   
+  // Randomly choose the computer's choice and decide who wins
+  var computerChoice = randomChoose(choices);
+  var winner = decideWinner(playerChoice, computerChoice);
 
-console.log(x);
+  // Update the screen with the correct icon for each player
+  setProperty("playerOutput","image",findIcon(playerChoice));
+  setProperty("computerOutput","image",findIcon(computerChoice));
+
+  
+  // Update the screen with the text explaining who won.
+  if(winner == "Draw"){
+    setProperty("gameText","text","Draw!");
+  } else if (winner == "Player") {
+    setProperty("gameText","text","You win!");
+    playerScore++;
+    setProperty("playerScoreOutput","text",playerScore);
+  } else if (winner == "Computer") {
+    setProperty("gameText","text","Computer wins!");
+    computerScore++;
+    setProperty("computerScoreOutput","text",computerScore);    
+  }
 }
+
+// iconName {string} - either "Rock", "Paper", or "Scissors"
+// return {string} - the icon associated with the string provided
+function findIcon(iconName){
+    var currentIcon = "";
+    if (iconName == "Rock"){
+      currentIcon == icons[0];
+    } else if (iconName == "Paper"){
+      currentIcon == icons[1];
+} else if (iconName == "Scissors"){
+      currentIcon == icons[2];
+}
+return currentIcon;
+}
+
+// Randomly selects an item from the list provided and returns it
+// list {list} - a list of items
+// return {item} - a randomly chosen item from the list
+function randomChoose(list){
+    var randomItem = list[(randomNumber(0, list.length - 1))];
+    return randomItem;
+}
+
+// Decides who wins a round of rock paper scissors by taking the two 
+// playerChoice {string} - either "Rock", "Paper", or "Scissors", the human player's choice
+// computerChoice {string} - either "Rock", "Paper", or "Scissors", the computer player's choice
+// return {string} - either "Draw", "Player", or "Computer", depending on who
+function decideWinner(playerChoice, computerChoice){
+  var result = "draw";
+  if (playerChoice == "Rock"){
+  if (computerChoice == "Scissors"){
+    result = "Player";
+  } else if(computerChoice == "Paper"){
+    result = "Computer";
+}
+  } else if (playerChoice == "Paper"  ){
+  
+  } else if (playerChoice == "Scissors"  ){
+}
+  
+  return "Player";
 }
