8ff25b80515c8cad4cefedf76b56cbd693894bd4
Littlelinkrocks <77841378+Littlelinkrocks@users.noreply.github.com>
2021-05-02 21:16:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,33 @@
-// List of icons that can be used in the game
-var icons = ["icon://fa-hand-rock-o", "icon://fa-hand-paper-o", "icon://fa-hand-scissors-o"];
 
-// List of possible choices to play
 var choices = ["Rock", "Paper", "Scissors"];
 
-// User and computer scores, started at 0
+
 var playerScore = 0;
 var computerScore = 0;
 
-// Button for player to play rock
+
 onEvent("rockButton","click",function(){
   updateScreen("Rock");
 });
 
-// Button for player to play paper
+
 onEvent("paperButton","click",function(){
   updateScreen("Paper");
 });
 
-// Button for player to play scissors
 onEvent("scissorsButton","click",function(){
   updateScreen("Scissors");
 });
 
-// When the user clicks any of the three buttons it runs a round
-// of rock paper scissors. This function determines who won the
-// game and updates the screen and scores for each player.
+
 function updateScreen(playerChoice){
   
-  // Randomly choose the computer's choice and decide who wins
+
   var computerChoice = randomChoose(choices);
   var winner = decideWinner(playerChoice, computerChoice);
 
-  // Update the screen with the correct icon for each player
-  setProperty("playerOutput","image",findIcon(playerChoice));
-  setProperty("computerOutput","image",findIcon(computerChoice));
-
   
-  // Update the screen with the text explaining who won.
+
   if(winner == "Draw"){
     setProperty("gameText","text","Draw!");
   } else if (winner == "Player") {
@@ -51,8 +41,7 @@ function updateScreen(playerChoice){
   }
 }
 
-// iconName {string} - either "Rock", "Paper", or "Scissors"
-// return {string} - the icon associated with the string provided
+
 function findIcon(iconName){
     var currentIcon = "";
     if (iconName == "Rock"){
@@ -65,18 +54,13 @@ function findIcon(iconName){
 return currentIcon;
 }
 
-// Randomly selects an item from the list provided and returns it
-// list {list} - a list of items
-// return {item} - a randomly chosen item from the list
+
 function randomChoose(list){
     var randomItem = list[(randomNumber(0, list.length - 1))];
     return randomItem;
 }
 
-// Decides who wins a round of rock paper scissors by taking the two 
-// playerChoice {string} - either "Rock", "Paper", or "Scissors", the human player's choice
-// computerChoice {string} - either "Rock", "Paper", or "Scissors", the computer player's choice
-// return {string} - either "Draw", "Player", or "Computer", depending on who
+
 function decideWinner(playerChoice, computerChoice){
   var result = "draw";
   if (playerChoice == "Rock"){
